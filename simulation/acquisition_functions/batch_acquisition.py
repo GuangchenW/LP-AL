Fixed batch acquisition, not it updates the variance of batch points correctly

simulation/acquisition_functions/batch_acquisition.py
@@ -3,9 +3,10 @@ import numpy as np
 from .acquisition_functions import AcquisitionFunction
 
 class Batch_Acquisition(AcquisitionFunction):
-    def __init__(self, utility_func="ULP", device="cpu"):
+    def __init__(self, model, utility_func="ULP", device="cpu"):
         if not utility_func in ["ULP"]:
             raise Exception("utility_func {utility_func} not supported for batch acquisition")
+        self.model = model
         super().__init__(utility_func=utility_func, device=device)
 
     def acquire(
@@ -32,5 +33,11 @@ class Batch_Acquisition(AcquisitionFunction):
 
             doe_input = np.append(doe_input, [input_population[min_id]], axis=0)
             doe_response = np.append(doe_response, [mean[min_id]])
+            #variance[min_id] = 0
+            k=i+1
+            mean, variance = self.model.fantasize(doe_input[-k:], doe_response[-k:], input_population)
+
+            #m, v = self.model.fantasize([[1,1],[2,2]], [10,5], [[1,1],[2,2]])
+            #print(m,v)
 
         return np.array(out)

simulation/custom_simulation.py
@@ -6,7 +6,7 @@ import math
 
 from ordinary_kriging import OrdinaryKriging
 
-from objective_functions import G_4B, G_Ras, G_hat
+from objective_functions import G_4B, G_2B, G_Ras, G_hat
 from acquisition_functions import Single_Acquisition, Batch_Acquisition
 from subset_samplers import U_Sampler
 
@@ -25,7 +25,7 @@ points = np.dstack((point_x1,point_x2))[0]
 #plt.show()
 
 # Objective function
-G = G_Ras
+G = G_2B
 
 # This is STEP2 "...a dozen points are enough"
 # Using points from MC samples instead
@@ -40,19 +40,16 @@ for i in range(N_INIT):
     x2 = x2_val[i]
     DOE[i, :] = np.array([x1, x2, G(x1, x2)])
 
-U = Batch_Acquisition(utility_func="ULP")
-sampler = U_Sampler(threshold=6)
-
 max_iter = 100
-final_kriging_model = None
 kriging_model = OrdinaryKriging()
+U = Batch_Acquisition(kriging_model, utility_func="ULP")
+sampler = U_Sampler(threshold=2)
 subset_samples = []
 p_failures = []
 for i in range(max_iter):
 
     # STEP3 Compute Kriging model
     kriging_model.train(DOE[:,:2], DOE[:,2])
-    final_kriging_model = kriging_model
     # STEP4 Estimate the probabilty of failure based on estimation of all points
     # in MC sample. P_f is calculated by P_f=N_{G<=0}/N_MC
 
@@ -101,6 +98,7 @@ for i in range(max_iter):
 # TODO: 9,10 for when one MC population is not enough
 
 # STEP8: Compute coefficient of variation of the probability of failure
+final_kriging_model = kriging_model
 z, ss = final_kriging_model.execute(points)
 num_negative_predictions = np.sum(z < 0)
 P_f = np.maximum(num_negative_predictions / N_MC, 0.0001)
@@ -145,7 +143,7 @@ plt.title('Kriging Interpolation')
 contours = plt.contour(grid_x, grid_y, z, levels=[0], colors='r', linewidths=2)
 
 # Plot the points queried
-plt.scatter(DOE[:, 0], DOE[:, 1], c='black', label='Data')
+plt.scatter(DOE[:, 0], DOE[:, 1], s=2, c='black', label='Data')
 # Label the points queried with their actual value
 #for x1, x2, h in DOE:
 #    plt.text(x1, x2, f'{h:.2f}', fontsize=8, color='white', ha='center', va='center')

simulation/ordinary_kriging.py
@@ -79,10 +79,19 @@ class OrdinaryKriging:
 
 	# TODO: Could be improved with fast_pred_var?
 	def execute(self, inputs):
-
 		with torch.no_grad():
 			inputs = torch.tensor(inputs)
 			f_preds = self.gp(inputs)
+			return (f_preds.mean.numpy(), f_preds.variance.numpy())
+
+	def fantasize(self, inputs, targets, tests):
+		noises = torch.ones(np.shape(inputs)[0])*0.001
+		inputs = torch.tensor(inputs)
+		targets = torch.tensor(targets)
+		tests = torch.tensor(tests)
+		model = self.gp.get_fantasy_model(inputs, targets, noise=noises)
+		with torch.no_grad():
+			f_preds = model(tests)
 			return (f_preds.mean.numpy(), f_preds.variance.numpy())
 
 if __name__ == "__main__":

simulation/utility_functions/utilities.py
@@ -20,30 +20,28 @@ def ULP(candidates, mean, variance, doe_input, doe_response):
 		util.append(u)
 	
 	max_d = np.max(dist)
-	max_u = np.max(util)
+
+	def slerp(x):
+		return 1/(1+np.exp(-10*(x-0.5)))
 
 	out = []
 	for d,u in zip(dist, util):
-		out.append(u/(d/max_d))
+		out.append(u/slerp(d/max_d))
 	return np.array(out)
 
 def LF(candidate, mean, variance, doe_input, doe_response):
 	if variance < 0.0001:
-		return 1000
+		return [0, 1000]
 
-	max_U = 0
-	min_U = 0
-	max_d = 0
+	min_U = 1000
 	min_d = np.inf
 	for i in range(len(doe_input)):
 		dist = np.linalg.norm(candidate - doe_input[i])
-		util = ULP_helper(candidate, doe_response[i], mean, variance)
 		#max_U = util if util > max_U else max_U
 		#max_d = dist if dist > max_d else max_d
 		if dist < min_d:
 			min_d = dist
-			min_U = util
-	#out = (target_U/max_U)/(min_d/max_d)
+			min_U = ULP_helper(candidate, doe_response[i], mean, variance)
 	return [min_d, min_U]
 
 def ULP_helper(candidate, perform_near, mean, variance):