Fixed gradient norm calculation

simulation/ak_mcs.py
@@ -16,7 +16,7 @@ class AKMCS:
 		self.sampler = sampler if not sampler == None else U_Sampler(threshold=4)
 		#self.sampler.aggressive_mode(True)
 		self.evaluator = evaluator if not evaluator == None else LP_Batch(acq_func=self.acq_func)
-		self.stopper = ESC(epsilon_thr=0.02)
+		self.stopper = ESC(epsilon_thr=0.01)
 		self.max_iter = max_iter
 		self.batch_size = batch_size
 
@@ -59,7 +59,7 @@ class AKMCS:
 		# Acquire all estimations
 		mean, variance, grad = self.model.execute(self.kriging_sample, with_grad=True)
 		# Compute max norm of expected gradient
-		max_grad = np.max(np.linalg.norm(grad))
+		max_grad = np.max(np.linalg.norm(grad, axis=1))
 		max_grad = max(0.25, max_grad)
 
 		# Compute stopping criterion
@@ -92,6 +92,7 @@ class AKMCS:
 		self.logger.log("Max Expected Gradient: %.4g" % max_grad)
 
 		# STEP5 Compute learning function on the population and identify best point
+		self.evaluator.set_grad(grad)
 		self.evaluator.set_L(max_grad)
 		batch = self.evaluator.obtain_batch(
 			subset_pop, 

simulation/evaluators/lp_batch_evaluator.py
@@ -9,6 +9,9 @@ class LP_Batch(BaseEvaluator):
 		super().__init__(acq_func=acq_func, logger=logger)
 		self.L = float("nan")
 
+	def set_grad(self, grad):
+		self.grad = grad
+
 	def set_L(self, L):
 		self.L = min(10,L)
 
@@ -23,20 +26,25 @@ class LP_Batch(BaseEvaluator):
 	):
 		batch = []
 		utilities = self.acq_func.acquire(subset_points, mean, variance, doe_input, doe_response)
+		utilities += 1e-10 # To prevent log0 undefined
 		utilities = np.log(utilities)
 
+		# TEST HACK
+		grad_norm = np.linalg.norm(self.grad, axis=1)
+
 		for i in range(min(n_points, len(subset_points))):
-			min_id = np.argmax(utilities)
+			max_id = np.argmax(utilities)
 			batch.append({
-				"next": subset_points[min_id],
-				"mean": mean[min_id],
-				"variance": variance[min_id],
-				"utility": utilities[min_id]
+				"next": subset_points[max_id],
+				"mean": mean[max_id],
+				"variance": variance[max_id],
+				"utility": utilities[max_id]
 				})
 
+			#self.L = grad_norm[max_id]
 			utilities = self.apply_hammer(subset_points, batch[-1], utilities)
-			#doe_input = np.append(doe_input, [subset_points[min_id]], axis=0)
-			#doe_response = np.append(doe_response, [mean[min_id]])
+			#doe_input = np.append(doe_input, [subset_points[max_id]], axis=0)
+			#doe_response = np.append(doe_response, [mean[max_id]])
 			#k=i+1
 			#mean, variance = self.model.fantasize(doe_input[-k:], doe_response[-k:], subset_points)
 

simulation/models/ordinary_kriging.py
@@ -11,7 +11,7 @@ from .model import BaseModel
 class OrdinaryKriging(BaseModel):
 	def __init__(self, n_dim, regression=None, correlation=None):
 		super().__init__()
-		self.regression = regression or regr_linear
+		self.regression = regression or regr_constant
 		self.correlation = correlation or corr_gauss
 		theta = np.ones(n_dim)
 		thetaL = theta * 1e-4